de59fbd2af35d12e2c4005c18878e9b0773b1f3a
raffaele <raffaele.linux@gmail.com>
2015-01-11 12:26:11 +0100

    cookies enabled

--- view.py
+++ view.py
@@ -1,55 +1,69 @@
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 from PyQt4.QtWebKit import *
+from PyQt4.QtNetwork import *
 import sys
 sys.path.append('ui/')
 from page import * 
 
+#cookie
+cookie = QNetworkCookieJar()
+mgr = QNetworkAccessManager()
+mgr.setCookieJar(cookie)
+
 class View(QWidget,Ui_page):
-  def __init__(self,parent,url):
-    super(View,self).__init__()
-    #to call tab window function
-    self.parent = parent
-    #number of tab of the current webview
-    self.index = 0
-    self.setupUi(self)
-    if url=="":
-    	self.qwebview.setUrl(QUrl('http://start.ubuntu.com'))
-    else:
-	self.qwebview.setUrl(QUrl(url))
-    QWebSettings.globalSettings().setAttribute(QWebSettings.PluginsEnabled, True)
-    #for icon connection
-    QWebSettings.setIconDatabasePath('data/')
- 
-  def viewLoading(self,value):
-   self.progressBar.setValue(value)
-   if value==100:
-     self.progressBar.setValue(0)
+	def __init__(self,parent,url):
+		super(View,self).__init__()
+		#to call tab window function
+		self.parent = parent
+		#number of tab of the current webview
+		self.index = 0
+		self.setupUi(self)
+		#cookies
+		page = self.qwebview.page()
+		page.setNetworkAccessManager(mgr)
+		print "cookie gestion enabled"
+		#plugin
+		QWebSettings.globalSettings().setAttribute(QWebSettings.PluginsEnabled, True)
+		#js
+		QWebSettings.globalSettings().setAttribute(QWebSettings.JavascriptEnabled, True)
+		#for icon connection
+		QWebSettings.setIconDatabasePath('data/')
+		QWebSettings.globalSettings().setLocalStoragePath('data/')
+		if url=="":
+			self.qwebview.setUrl(QUrl('http://start.ubuntu.com'))
+		else:
+			self.qwebview.setUrl(QUrl(url))
+
+	def viewLoading(self,value):
+		self.progressBar.setValue(value)
+		if value==100:
+			self.progressBar.setValue(0)
      
-  def viewReload(self):
-    self.qwebview.reload()
+	def viewReload(self):
+		self.qwebview.reload()
     
-  def viewStop(self):
-    self.qwebview.stop()
+	def viewStop(self):
+		self.qwebview.stop()
     
-  def viewForward(self):
-    self.qwebview.forward()
+	def viewForward(self):
+		self.qwebview.forward()
   
-  def viewBack(self):
-    self.qwebview.back()
+	def viewBack(self):
+		self.qwebview.back()
     
-  def viewHome(self):
-    self.qwebview.setUrl(QUrl('http://start.ubuntu.com'))
+	def viewHome(self):
+		self.qwebview.setUrl(QUrl('http://start.ubuntu.com'))
     
-  def viewTitle(self,title):
-    self.parent.tabTitle(title,self.index)
+	def viewTitle(self,title):
+		self.parent.tabTitle(title,self.index)
     
-  def viewIcon(self):
-    self.parent.tabIcon(self.qwebview.icon(),self.index)
+	def viewIcon(self):
+		self.parent.tabIcon(self.qwebview.icon(),self.index)
     
-  def viewUrl(self,url):
-   self.lineEdit.setText(url.toString())
+	def viewUrl(self,url):
+		self.lineEdit.setText(url.toString())
    
-  def viewGo(self):
-    url = self.lineEdit.text()
-    self.qwebview.setUrl(QUrl(url))
+	def viewGo(self):
+		url = self.lineEdit.text()
+		self.qwebview.setUrl(QUrl(url))
